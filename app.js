10a47f56257727517bfe5640564855738f8dfd49
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-03 20:54:07 -0500

    working on hiding/showing sections

--- app.js
+++ app.js
@@ -14,24 +14,15 @@ $(window).load(function() {
 
 });
 
-$('a').on('click', function(e) {
+//I want to use a variable here so that the appropriate album page whill load when clicked on.  
+//I have heard that data attributes will work, I dont understand how.  
 
+$('a').on('click', function(e) {
 
 	$('.listview').hide();
 	$('.albumview').show();
 
 })
-
-//This is example from this week, Nav Bar
-// $('.links a').click(function(e) {
-// 	e.preventDefault();
-// 	console.dir(e.target);
-// 	$('.links a').css({'background-color': 'transparent'});
-// 	$(e.target).css({'background-color': 'white'});
-		
-// })
-
-
 
 
 /* User is shown the album view.  When click on a album button, that button highlights and user is taken
