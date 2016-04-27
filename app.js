f249c31fa4309716e6a073cbe8abed873de3b16c
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-27 14:35:13 -0500

    added function renderAlbum to dynamically render each photo album

--- app.js
+++ app.js
@@ -1,41 +1,82 @@
 
 /* User is first shown listview.  When click on an album, shown the corresponding album view.
-* Three parts of event handling:
+* On the album view,  when click on a album button, that button highlights and user is shown
+* to the corresponding album.  If user selects a photo, shown the corresponding photo view.
+*/
+
+/* Event listener process:  
 * 1. Select the element that you want to listen for events on.
 * 2. Add the event listener.
 * 3. Create a function to run when that event happens
 */
 
+var albums = [
+	{
+    title: 'cool cats',
+    photos: [
+      {
+        title: 'cat in a box',
+        url: 'http://catinabox.com'
+      } 
+
+     ]
+  	},
+
+  	{
+    title: 'chicago'
+    photos: [
 
 
-//first hide the other sections when page loads
+    ]
+  	},
 
-// $(window).load(function() {
-//   	$('.albumview').hide();
-// 	$('.photoview').hide();  
 
-// });
-
-// //I want to use a variable here so that the appropriate album page whill load when clicked on.  
-// //I have heard that data attributes will work, I dont understand how.  
-
-//**Maybe use jQuery .toggle() function to toggle album buttons.  First turn all buttons off and 
-//then highlight only one that was clicked.  Clicked one should show correct album.  
+]
 
 
 
+//adds an event handler on the album buttons and then renders the clicked Album based on the albumNumber.
 
-// $('a').on('click', function(e) {
+$('albumSelector button').on('click', function(e){
+	var albumNumber = $(e.target).data('albumNumber');
+	renderAlbum(albumNumber);
+});
 
-// 	$('.listview').hide();
-// 	$('.albumview').show();
+//renders the Album, based on the album number.
+//puts the html on the page with the data for that album
+function renderAlbum (albumNumber) {
+	var thisAlbum = albums[albumNumber];
+	var element = var element = $('<section/>')
+  		.addClass('album')
+  		.append('<header><h1>' + thisAlbum.title + '</h1></header>')
+  		.append(thisAlbum.photos.map(function(photo, i) {
+    return '<div class="photoholder"><img src="'+ photo.url +'"><div class="label"><a href="#">Photo '+ i + '</a></div>'
+  }));
 
-// })
+$('body').html(element);
+registerPhotoLabelClick();
 
 
-/* User is shown the album view.  When click on a album button, that button highlights and user is taken
-* to the corresponding album.  If user selects a photo, taken to corresponding photo view.
-*/
+
+						
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
